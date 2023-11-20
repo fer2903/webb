d87884b30ca6f00853dcb9912723fbca00abd753
fer2903 <fer2903@hotmail.es>
2023-11-20 00:52:46 -0600

    web

---
+++ src/constants/index.js
@@ -0,0 +1,261 @@
+import { sfleet, nextCloud, spacebar, pharos } from "../assets/images";
+import {
+    car,
+    contact,
+    css,
+    estate,
+    express,
+    git,
+    github,
+    html,
+    javascript,
+    linkedin,
+    mongodb,
+    mui,
+    nextjs,
+    nodejs,
+    pricewise,
+    react,
+    redux,
+    sass,
+    snapgram,
+    summiz,
+    threads,
+    typescript,
+    ruby,
+    rails,
+    nest,
+    flask,
+    python,
+    postgres,
+    postman,
+    aws
+} from "../assets/icons";
+
+export const skills = [
+    {
+        imageUrl: ruby,
+        name: "Ruby",
+        type: "Backend",
+    },
+    {
+        imageUrl: rails,
+        name: "Rails",
+        type: "Backend",
+    },
+    {
+        imageUrl: typescript,
+        name: "TypeScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nest,
+        name: "Nestjs",
+        type: "Backend",
+    },
+    {
+        imageUrl: nodejs,
+        name: "Node.js",
+        type: "Backend",
+    },
+    {
+        imageUrl: javascript,
+        name: "JavaScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: express,
+        name: "Express",
+        type: "Backend",
+    },
+    {
+        imageUrl: python,
+        name: "Python",
+        type: "Backend",
+    },
+    {
+        imageUrl: flask,
+        name: "flask",
+        type: "Backend",
+    },
+    {
+        imageUrl: postgres,
+        name: "Postgresql",
+        type: "Database",
+    },
+    {
+        imageUrl: mongodb,
+        name: "MongoDB",
+        type: "Database",
+    },
+    {
+        imageUrl: html,
+        name: "HTML",
+        type: "Frontend",
+    },
+    {
+        imageUrl: css,
+        name: "CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: mui,
+        name: "Material-UI",
+        type: "Frontend",
+    },
+  
+    {
+        imageUrl: react,
+        name: "React",
+        type: "Frontend",
+    },
+    {
+        imageUrl: redux,
+        name: "Redux",
+        type: "State Management",
+    },
+    {
+        imageUrl: sass,
+        name: "Sass",
+        type: "Frontend",
+    },
+    {
+        imageUrl: git,
+        name: "Git",
+        type: "Version Control",
+    },
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+    {
+        imageUrl: postman,
+        name: "Postman",
+        type: "Api",
+    },
+    {
+        imageUrl: aws,
+        name: "Aws",
+        type: "DevOps",
+    },
+];
+
+export const experiences = [
+    {
+        title: "Backend Developer",
+        company_name: "Sfleet",
+        icon: sfleet,
+        iconBg: "#FFFFFF",
+        date: "May 2018 - sep 2020",
+        points: [
+            "Developing and maintaining web applications using Ruby on rails, React js and postgresql.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "React Native Developer",
+        company_name: "Next cloud",
+        icon: nextCloud,
+        iconBg: "#000000",
+        date: "sep 2020 - jul 2021",
+        points: [
+            "Developing a mobile App with react native, mssql an coldfusion",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "Web Developer",
+        company_name: "Spacebar",
+        icon: spacebar,
+        iconBg: "#a2d2ff",
+        date: "March 2021 - Apr 2022",
+        points: [
+            "Developing and maintaining e-comerce appllications using React.js, node js, express js, ruby, and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "Full stack Developer",
+        company_name: "Meta",
+        icon: pharos,
+        iconBg: "#FFFFFF",
+        date: "Jun 2021 - Present",
+        points: [
+            "Developing and maintaining Backoffice platform maintenance usign: Ruby on rails, postgresql, Python, C#.",
+            "Productive deployment: Aws (EC2, S3, lambda)",
+            "Architecture design of new web developments: Nest js, Postgresql.",
+            "Architecture design of new mobile developments: Flutter, nest js Api",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+];
+
+export const socialLinks = [
+    {
+        name: 'Contact',
+        iconUrl: contact,
+        link: '/contact',
+    },
+    {
+        name: 'GitHub',
+        iconUrl: github,
+        link: 'https://github.com/fer2903',
+    },
+    {
+        name: 'LinkedIn',
+        iconUrl: linkedin,
+        link: 'https://www.linkedin.com/in/fernando-vargas-811505226/',
+    }
+];
+
+export const projects = [
+    {
+        iconUrl: pricewise,
+        theme: 'btn-back-red',
+        name: 'Proyect Parewi',
+        description: 'One of the founders of Parewi, a foundation that helps marginalized children in Oaxaca, Mexico, We helped many children during my university days',
+        link: 'https://github.com/pywirrarika',
+    },
+    {
+        iconUrl: threads,
+        theme: 'btn-back-green',
+        name: 'Application for kids',
+        description: 'This application helps children with language issues',
+        link: 'https://github.com/fer2903/app_for_kids',
+    },
+    {
+        iconUrl: car,
+        theme: 'btn-back-blue',
+        name: 'React js frontend',
+        description: 'Design a social network that helps motorcyclists, obtaining information in case of an accident',
+        link: 'https://github.com/fer2903/motohub_front_react',
+    },
+    {
+        iconUrl: snapgram,
+        theme: 'btn-back-pink',
+        name: 'Backend Api rails',
+        description:  'Design a social network that helps motorcyclists, obtaining information in case of an accident',
+        link: 'https://github.com/fer2903/motohub_api_rails',
+    },
+    {
+        iconUrl: estate,
+        theme: 'btn-back-black',
+        name: 'Python help notes',
+        description: 'I have some help repositories if you are learning python, download them they contain the fundamentals and some interesting examples',
+        link: 'https://github.com/fer2903/python_basics',
+    },
+    {
+        iconUrl: summiz,
+        theme: 'btn-back-yellow',
+        name: 'algorithms',
+        description: 'developed a couple of classes that will help you in the study of directional and unidirectional graphs. I will upload a visual version of it soon',
+        link: 'https://github.com/fer2903/python_grapho'
+    }
+];
